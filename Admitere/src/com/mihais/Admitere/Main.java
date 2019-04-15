8e47f6aabcd395ea41dd06bcdde5b66ca26a5bf5
MihailSandulescu <49581007+MihailSandulescu@users.noreply.github.com>
2019-04-15 23:57:40 +0300

    First commit - Finished models
    - all models are implemented
    - started working on basic functionality services for each model

---
+++ Admitere/src/com/mihais/Admitere/Main.java
@@ -0,0 +1,8 @@
+package com.mihais.Admitere;
+
+public class Main {
+
+    public static void main(String [] args){
+
+    }
+}
---
+++ Admitere/src/com/mihais/Admitere/Models/Persoane/Persoana.java
@@ -0,0 +1,66 @@
+package com.mihais.Admitere.Models.Persoane;
+
+public abstract class Persoana{
+
+    protected String nume;
+    protected int varsta;
+    protected String CNP;
+    protected String email;
+
+    public Persoana() {}
+
+    public Persoana(String nume, int varsta, String CNP, String email){
+        this.nume = nume;
+        this.varsta = varsta;
+        this.CNP = CNP;
+        this.email = email;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public int getVarsta() {
+        return varsta;
+    }
+
+    public void setVarsta(int varsta) {
+        this.varsta = varsta;
+    }
+
+    public String getCNP() {
+        return CNP;
+    }
+
+    public void setCNP(String CNP) {
+        this.CNP = CNP;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Persoana)) return false;
+        Persoana persoana = (Persoana) o;
+        return this.getCNP().equals(persoana.getCNP());
+    }
+
+    @Override
+    public int hashCode() {
+        //metoda preluata din Effective Java (item 9)
+        int cod = 17;
+        cod *= 31 + CNP.hashCode();
+        return cod;
+    }
+}
